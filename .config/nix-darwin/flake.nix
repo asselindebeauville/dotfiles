276723f5c76d57e4913a2f49c1eaa21daa35a8ec
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2025-10-15 16:28:42 +0200

    nix-darwin: add git

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -27,6 +27,7 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages = [
+        pkgs.git
       ];
 
       # Manage Homebrew packages.
