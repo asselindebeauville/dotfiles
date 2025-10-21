08c560147a5a6a575ca602562478abd6554426e5
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2025-10-22 00:07:10 +0200

    nvim: add LuaLS and lazydev.nvim
    lazydev.nvim is a plugin that properly configures LuaLS for editing
    Neovim config by lazily updating workspace libraries.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,7 +3,24 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
-      { "mason-org/mason-lspconfig.nvim", opts = {} },
+      {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {
+          ensure_installed = {
+            "lua_ls",
+          },
+        },
+      },
+    },
+  },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
     },
   },
 }
