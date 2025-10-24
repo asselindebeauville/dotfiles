48e814f6f11366863ebf4b0269bb5e8f80fc8912
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2025-10-24 23:59:45 +0200

    nvim: add keymap to format buffer

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,6 +3,16 @@ return {
     "stevearc/conform.nvim",
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<Leader>f",
+        function()
+          require("conform").format({ async = true })
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
     ---@module "conform"
     ---@type conform.setupOpts
     opts = {
