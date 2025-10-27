aec2d8fac8026c0c1f8d802a2e18d55429969694
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2025-10-27 21:34:28 +0100

    nix-darwin: format flake.nix

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -18,87 +18,102 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, nix-darwin, nix-homebrew, ... }:
-  let
-    configuration = { pkgs, ... }: {
-      # Set the primary user.
-      system.primaryUser = "kyllian";
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nix-darwin,
+      nix-homebrew,
+      ...
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          # Set the primary user.
+          system.primaryUser = "kyllian";
 
-      # Set environment variables.
-      environment.variables = {
-        EDITOR = "nvim";
-      };
-
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = [
-        pkgs.git
-        pkgs.neovim
-        pkgs.nixfmt
-        pkgs.stow
-      ];
-
-      # Manage Homebrew packages.
-      homebrew = {
-        enable = true;
-        onActivation.cleanup = "zap";
-
-        casks = [
-          "colemak-dh"
-          "ghostty"
-        ];
-      };
-
-      # Configure keyboard mappings.
-      system.keyboard = {
-        enableKeyMapping = true;
-        userKeyMapping = [
-          {
-            HIDKeyboardModifierMappingSrc = 30064771129; # Caps Lock
-            HIDKeyboardModifierMappingDst = 30064771114; # Backspace
-          }
-        ];
-      };
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Kyllians-MacBook-Pro
-    darwinConfigurations."Kyllians-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [
-        configuration
-        nix-homebrew.darwinModules.nix-homebrew
-        ({ config, ... }: {
-          nix-homebrew = {
-            enable = true;
-            user = config.system.primaryUser;
-            taps = {
-              "homebrew/homebrew-core" = inputs.homebrew-core;
-              "homebrew/homebrew-cask" = inputs.homebrew-cask;
-            };
-            mutableTaps = false;
+          # Set environment variables.
+          environment.variables = {
+            EDITOR = "nvim";
           };
-        })
-        ({ config, ... }: {
-          homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
-        })
-      ];
+
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.git
+            pkgs.neovim
+            pkgs.nixfmt
+            pkgs.stow
+          ];
+
+          # Manage Homebrew packages.
+          homebrew = {
+            enable = true;
+            onActivation.cleanup = "zap";
+
+            casks = [
+              "colemak-dh"
+              "ghostty"
+            ];
+          };
+
+          # Configure keyboard mappings.
+          system.keyboard = {
+            enableKeyMapping = true;
+            userKeyMapping = [
+              {
+                HIDKeyboardModifierMappingSrc = 30064771129; # Caps Lock
+                HIDKeyboardModifierMappingDst = 30064771114; # Backspace
+              }
+            ];
+          };
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Kyllians-MacBook-Pro
+      darwinConfigurations."Kyllians-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          (
+            { config, ... }:
+            {
+              nix-homebrew = {
+                enable = true;
+                user = config.system.primaryUser;
+                taps = {
+                  "homebrew/homebrew-core" = inputs.homebrew-core;
+                  "homebrew/homebrew-cask" = inputs.homebrew-cask;
+                };
+                mutableTaps = false;
+              };
+            }
+          )
+          (
+            { config, ... }:
+            {
+              homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
+            }
+          )
+        ];
+      };
     };
-  };
 }
