ae63aef2207972cec1e3d99909260b27e8399255
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2026-01-02 22:14:00 +0100

    nvim: add window navigation keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,6 @@
 vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
+
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>", { desc = "Go to the left window" })
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>", { desc = "Go to the down window" })
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>", { desc = "Go to the up window" })
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>", { desc = "Go to the right window" })
